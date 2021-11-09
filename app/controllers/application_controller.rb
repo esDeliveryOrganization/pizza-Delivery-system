724b56172cd2048eb16ee34730b55ab3fae1a3ed
Breno Macedo Barkokebas <49546135+brenomacedodm@users.noreply.github.com>
2021-11-09 20:54:07 -0300

    Mudanca da chave de autenticacao do devise
    Mudanca da chave de autenticacao de email para cpf

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,22 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nome])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:cpf])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:telefone])
+  end
+
+  def configure_account_update_params
+    devise_parameter_sanitizer.permit(:account_update, keys: [:nome])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:cpf])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:telefone])
+  end
+
+  def configure_sign_in_params
+    devise_parameter_sanitizer.permit(:sign_in, keys: [:cpf])
+  end
+
 end
