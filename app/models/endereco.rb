7668dd86f53a7d5be783ce20a6b5d2a71273ef72
jacksonlmp <jacksonlmp17.jl@gmail.com>
2021-11-12 18:11:49 -0300

    Validates de endereco

--- app/models/endereco.rb
+++ app/models/endereco.rb
@@ -1,5 +1,10 @@
 class Endereco < ApplicationRecord
   has_one :user
 
+  validates :cep, presence: true, numericality: { only_integer: true }, length: {is: 8}
+  validates :cidade, presence: true, length: {minimum: 4, too_short: "Must have at least 4 characters!"}
+  validates :bairro, presence: true, length: {minimum: 4, too_short: "Must have at least 4 characters!"}
+  validates :logradouro, presence: true, length: {minimum: 4, too_short: "Must have at least 4 characters!"}
+  validates :complemento, length: {maximum:50, too_long: "Must have at maximum 50 characters!"}
 
 end
