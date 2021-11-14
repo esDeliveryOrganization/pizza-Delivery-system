1e5b4e72ac2ce1a737ee56a59c785055dfd723a3
Luiz Davi <55983920+luiz-davi@users.noreply.github.com>
2021-11-13 23:23:11 -0300

    Implementando a funcionalidade: cadastrar usuários(clientes ou adm) para adm

--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,6 @@ module PizzaDeliverySystem
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    # config.paths['app/views'] << "app/views/devise"
   end
 end
