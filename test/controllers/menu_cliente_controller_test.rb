3e5e104a8caf9f33138b6c0c2a09d187748c6442
Luiz Davi <55983920+luiz-davi@users.noreply.github.com>
2021-11-15 17:56:01 -0300

    Corrigindo arquivo travis

--- test/controllers/menu_cliente_controller_test.rb
+++
@@ -1,8 +0,0 @@
-require "test_helper"
-
-class MenuClienteControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get menu_cliente_index_url
-    assert_response :success
-  end
-end
