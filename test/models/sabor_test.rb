16839c3542d4b95427efc0ccb9b1a38c81fea411
Thiago Cavalcanti Silva <tcavalcanti.tc@gmail.com>
2021-11-15 13:06:33 -0300

    Implementando testes de model de sabor

--- test/models/sabor_test.rb
+++ test/models/sabor_test.rb
@@ -1,7 +1,21 @@
-require "test_helper"
+require 'test_helper'
 
 class SaborTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  # Cadastro valido
+  test 'sabor deve ser salvo' do
+    sabor = Sabor.new nome: 'Sabor de teste', preco: '30.0'
+    assert sabor.save
+  end
+
+  # Cadastro invalido (viola validates de nome)
+  test 'nao deve cadastrar sabor sem nome' do
+    sabor = Sabor.new nome: '', preco: '30.0'
+    assert_not sabor.save
+  end
+
+  # Cadastro invalido (viola validates de preco)
+  test 'nao deve cadastrar sabor sem preco' do
+    sabor = Sabor.new nome: 'Sabor de teste', preco: ''
+    assert_not sabor.save
+  end
 end
