08fa1bdcb3f0ff2adb351caf6a28a7244305791b
Thiago Cavalcanti Silva <tcavalcanti.tc@gmail.com>
2021-11-15 04:19:36 -0300

    Corrigindo persistencia no banco e dados dos testes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,21 +4,21 @@ class ApplicationController < ActionController::Base
 
   protected
 
-  # def configure_permitted_parameters
-  #   devise_parameter_sanitizer.permit(:sign_up, keys: [:nome])
-  #   devise_parameter_sanitizer.permit(:sign_up, keys: [:cpf])
-  #   devise_parameter_sanitizer.permit(:sign_up, keys: [:telefone])
-  #   devise_parameter_sanitizer.permit(:sign_up, keys: [:email])
-  #   devise_parameter_sanitizer.permit(:sign_up, keys: [:adm])
-  # end
-  #
-  # def configure_account_update_params
-  #   devise_parameter_sanitizer.permit(:account_update, keys: [:nome])
-  #   devise_parameter_sanitizer.permit(:account_update, keys: [:cpf])
-  #   devise_parameter_sanitizer.permit(:account_update, keys: [:telefone])
-  #   devise_parameter_sanitizer.permit(:sign_up, keys: [:email])
-  #   devise_parameter_sanitizer.permit(:account_update, keys: [:adm])
-  # end
+   def configure_permitted_parameters
+     devise_parameter_sanitizer.permit(:sign_up, keys: [:nome])
+     devise_parameter_sanitizer.permit(:sign_up, keys: [:cpf])
+     devise_parameter_sanitizer.permit(:sign_up, keys: [:telefone])
+     devise_parameter_sanitizer.permit(:sign_up, keys: [:email])
+     devise_parameter_sanitizer.permit(:sign_up, keys: [:adm])
+   end
+
+   def configure_account_update_params
+     devise_parameter_sanitizer.permit(:account_update, keys: [:nome])
+     devise_parameter_sanitizer.permit(:account_update, keys: [:cpf])
+     devise_parameter_sanitizer.permit(:account_update, keys: [:telefone])
+     devise_parameter_sanitizer.permit(:sign_up, keys: [:email])
+     devise_parameter_sanitizer.permit(:account_update, keys: [:adm])
+   end
 
   def configure_sign_in_params
     devise_parameter_sanitizer.permit(:sign_in, keys: [:cpf])
--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,11 @@ Rails.application.routes.draw do
   get 'menu_cliente/index'
   resources :enderecos
   root 'home#index'
-  devise_for :users, controllers: { registrations: "registrations" }
+
   devise_scope :user do
     get '/signup', to: 'users/registrations#new', as: :inactive_signup
+    post '/users', to: 'users/registrations#create'
   end
-  
+  devise_for :users, controllers: { registrations: "registrations" }
   resources :sabors
 end
