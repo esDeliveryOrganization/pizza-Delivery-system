a104da5db75c22165009a5605297b0660779384c
Luiz Davi <55983920+luiz-davi@users.noreply.github.com>
2021-11-15 18:43:18 -0300

    Corrigindo problema do sing out

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,10 @@ Rails.application.routes.draw do
   get 'menu_cliente/index'
   resources :enderecos
   root 'home#index'
-
   devise_scope :user do
     get '/signup', to: 'users/registrations#new', as: :inactive_signup
     post '/users', to: 'users/registrations#create'
+    get '/users/sign_out', to: 'users/sessions#destroy', as: :destroy_user_session_path
   end
   devise_for :users, controllers: { registrations: "registrations" }
   resources :sabors
