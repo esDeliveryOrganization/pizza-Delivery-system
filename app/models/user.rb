69764f5f387e364f79c334c00070bf68229b66a8
jacksonlmp <jacksonlmp17.jl@gmail.com>
2021-11-17 19:56:22 -0300

    Corrigindo issue

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,9 +7,9 @@ class User < ApplicationRecord
          authentication_keys: [:cpf]
 
   validates :cpf, length: {is: 11}, presence: true, uniqueness: true,numericality: { only_integer: true }
-  validates :nome, presence: true, length: {minimum: 3, too_short: "must have at least 3 characters "}, length: { maximum: 30, too_long: "must have at maximum 30 characters" }, format: {with: /[a-zA-Z]/}
+  validates :nome, presence: true, length: {minimum: 3, too_short: "must have at least 3 characters "}, length: { maximum: 30, too_long: "must have at maximum 30 characters" }, format: {with: /\Ahttps?:\/\/[^\n]+\z/i}
   validates :telefone, presence: true, numericality: { only_integer: true }, length: {minimum: 8, too_short: "must have at least 8 numbers "}
   validates :password_confirmation, presence: true
-  validates :email, format: {with: /.com/}
+  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP } 
 
 end
