f6e4d9b722ef86ddc637954d2ce4abd5740ddb89
Luiz Davi <55983920+luiz-davi@users.noreply.github.com>
2021-11-18 20:16:53 -0300

    Consertando testes Unitários

--- test/models/endereco_test.rb
+++ test/models/endereco_test.rb
@@ -1,21 +1,27 @@
 require "test_helper"
 
 class EnderecoTest < ActiveSupport::TestCase
-  # Cadastro valido
+  usuario = User.new cpf: '12312312390', password: '123456', password_confirmation: '123456', nome: 'Usuario de teste', telefone: '87991929394', email: 'usuariodeteste@gmail.com'
+
   test 'endereco deve ser salvo' do
-    endereco = Endereco.new cep: '55244210', cidade: 'Cidade de teste', bairro: 'Bairro B', logradouro: 'Centro', complemento: 'Casa 20'
+    assert usuario.save
+    endereco = Endereco.new cep: '55244210', cidade: 'Cidade de teste', bairro: 'Bairro B', logradouro: 'Centro', complemento: 'Casa 20', user_id: usuario.id
     assert endereco.save
   end
 
   # Cadastro invalido (viola validade de CEP)
   test 'endereco nao deve ser salvo sem CEP' do
-    endereco = Endereco.new cep: '', cidade: 'Cidade de teste', bairro: 'Bairro B', logradouro: 'Centro', complemento: 'Casa 20'
+    usuario = User.new cpf: '12345678956', password: '123456', password_confirmation: '123456', nome: 'Usuario de teste', telefone: '87991929394', email: 'usuariodeteste@gmail.com'
+    assert usuario.save
+    endereco = Endereco.new cep: '', cidade: 'Cidade de teste', bairro: 'Bairro B', logradouro: 'Centro', complemento: 'Casa 20', user_id: usuario.id
     assert_not endereco.save
   end
 
   # Edicao valida
   test 'endereco deve ser alterado' do
-    endereco = Endereco.new cep: '55244210', cidade: 'Cidade de teste', bairro: 'Bairro B', logradouro: 'Centro', complemento: 'Casa 20'
+    usuario = User.new cpf: '12345678930', password: '123456', password_confirmation: '123456', nome: 'Usuario de teste', telefone: '87991929394', email: 'usuariodeteste@gmail.com'
+    assert usuario.save
+    endereco = Endereco.new cep: '55244210', cidade: 'Cidade de teste', bairro: 'Bairro B', logradouro: 'Centro', complemento: 'Casa 20', user_id: usuario.id
     assert endereco.save
     assert endereco.update cep: '55317504', cidade: 'Outra cidade', bairro: 'Outro bairro', logradouro: 'Avenida principal', complemento: 'Apto 303'
   end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,13 +3,13 @@ require 'test_helper'
 class UserTest < ActiveSupport::TestCase
   # Cadastro valido
   test 'usuario deve ser salvo' do
-    usuario = User.new cpf: '12312312390', password: '123456', password_confirmation: '123456', nome: 'Usuario de teste', telefone: '87991929394', email: 'usuariodeteste@gmail.com'
+    usuario = User.new cpf: '70496006444', password: '123456', password_confirmation: '123456', nome: 'Usuario de teste', telefone: '87991929394', email: 'usuariodeteste@gmail.com'
     assert usuario.save
   end
 
   # Apenas um usuario e criado
   test 'cadastra apenas um usuario' do
-    usuario = User.new cpf: '12312312390', password: '123456', password_confirmation: '123456', nome: 'Usuario de teste', telefone: '87991929394', email: 'usuariodeteste@gmail.com'
+    usuario = User.new cpf: '70496006444', password: '123456', password_confirmation: '123456', nome: 'Usuario de teste', telefone: '87991929394', email: 'usuariodeteste@gmail.com'
     assert usuario.save
     usuarios = User.all
     assert(usuarios.length == 1)
