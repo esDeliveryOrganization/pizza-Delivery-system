b677f658c7a8c30b6928c3f90b931b8e8ca0f18e
Thiago Cavalcanti Silva <tcavalcanti.tc@gmail.com>
2021-11-29 20:59:03 -0300

    Adicionando mais dois cenarios da feature controlarLoja

--- features/step_definitions/controlarLoja_steps.rb
+++ features/step_definitions/controlarLoja_steps.rb
@@ -1,4 +1,3 @@
-#Scenario Positivo 1
 When('eu clico em editar um pedido com ID ‘{int}’') do |id|
     expect(page).to have_content(id)
     click_link "Edit"
@@ -8,12 +7,39 @@ When('altero o status da pizza para {string}') do |_status|
     choose('pedido_status_entregue')
 end
 
-#Scenario Positivo 2 
 Then('Eu vejo um pedido com status {string}') do |status|
     expect(page).to have_content(status)
 end
 
-#Scenario Positivo 3 
 When('eu altero o entregador para {string}') do |entregador|
     select entregador, from:'pedido[entregador_id]'
+end
+
+Given('estou na pagina do horario da loja') do
+    visit('/lojas/1')
+    expect(page).to have_current_path('/lojas/1')
+end
+
+When('clico em editar') do
+    click_link "Edit"
+end
+
+When('preencho horario de abertura com {string}') do |horario|
+    fill_in 'loja_abertura', with:horario
+end
+
+When('preencho horario de fechamento com {string}') do |horario|
+    fill_in 'loja_fechamento', with:horario
+end
+
+When('clico em atualizar loja') do
+    click_button "Update Loja"
+end
+
+Then('vejo mensagem de loja atualizada com sucesso') do
+    expect(page).to have_content('Loja was successfully updated.')
+end
+
+Then('vejo mensagem de horario invalido') do
+    expect(page).to have_content('Hora de fechamento não pode ser antes da hora de abertura!')
 end
