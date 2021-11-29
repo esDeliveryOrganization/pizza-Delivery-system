35384bc57cc135b66e1220179e76bc52bccecaa8
Thiago Cavalcanti Silva <tcavalcanti.tc@gmail.com>
2021-11-29 20:58:23 -0300

    Adicionando validacao do horario de funcionamento

--- app/models/loja.rb
+++ app/models/loja.rb
@@ -1,2 +1,13 @@
 class Loja < ApplicationRecord
+  validate :validarFuncionamento
+  validates :abertura, presence: true
+  validates :fechamento, presence: true
+
+  def validarFuncionamento
+    if !fechamento.nil? && !abertura.nil?
+      if fechamento < abertura
+        errors.add(:fechamento, "Hora de fechamento não pode ser antes da hora de abertura!")
+      end
+    end
+  end
 end
