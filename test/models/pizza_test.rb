b87c7266a4548cd0ba0f0b0d7539fc38a9610a7a
Thiago Cavalcanti Silva <tcavalcanti.tc@gmail.com>
2021-11-28 23:28:13 -0300

    Adicionando testes unitarios de pizza

--- test/models/pizza_test.rb
+++ test/models/pizza_test.rb
@@ -1,7 +1,27 @@
 require "test_helper"
 
 class PizzaTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  # Cadastro invalido (viola validate de sabor)
+  test 'pizza nao deve ser salva sem sabor' do
+    pizza = Pizza.new tamanho: '1', sabor1_id: nil, sabor2_id: nil, fatias: '6', preco: '15.0', desc: 'Sem descricao', pedido_id: '1'
+    assert_not pizza.save
+  end
+
+  # Cadastro invalido (viola validate de tamanho)
+  test 'pizza nao deve ser salva sem tamanho' do
+    sabor = Sabor.new nome: 'Sabor de teste', preco: '30.0'
+    assert sabor.save
+    pizza = Pizza.new sabor1_id: sabor.id, sabor2_id: nil, fatias: '6', preco: '15.0', desc: 'Sem descricao', pedido_id: '1'
+    assert_not pizza.save
+  end
+
+  # Cadastro invalido (deve estar vinculada a pedido)
+  test 'pizza nao deve ser salva sem pedido' do
+    sabor = Sabor.new nome: 'Sabor de teste', preco: '30.0'
+    assert sabor.save
+    pizza = Pizza.new sabor1_id: sabor.id, sabor2_id: nil, fatias: '6', preco: '15.0', desc: 'Sem descricao'
+    assert_not pizza.save
+  end
+
 end
