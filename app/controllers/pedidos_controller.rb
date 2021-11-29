d32cc05ffcb7417c876febfc59c0912a22b3dbb3
Luiz Davi <55983920+luiz-davi@users.noreply.github.com>
2021-11-29 19:29:26 -0300

    Resolvendo issues codacy

--- app/controllers/pedidos_controller.rb
+++ app/controllers/pedidos_controller.rb
@@ -137,7 +137,7 @@ class PedidosController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_pedido
-      @pedido = Pedido.find(params[user_id: current_user.id])
+      @pedido = Pedido.find(params[:id])
     end
 
     # Only allow a list of trusted parameters through.
