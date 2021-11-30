95eec3236ebd325734f4b302fe73697466480793
Luiz Davi <55983920+luiz-davi@users.noreply.github.com>
2021-11-29 21:45:50 -0300

    Teste unitários do model loja

--- test/models/loja_test.rb
+++ test/models/loja_test.rb
@@ -1,7 +1,23 @@
 require "test_helper"
 
 class LojaTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  require 'test_helper'
+  # Cadastro valido
+  test 'loja deve ser salva' do
+    loja = Loja.new abertura: '18:00', fechamento: '23:59'
+    assert loja.save
+  end
+
+  # Cadastro invalido (viola validates de abertura)
+  test 'cadastar loja com horario invalido' do
+    loja = Loja.new abertura: '', fechamento: '20:00'
+    assert_not loja.save
+  end
+
+  # Edição invalida (viola validates de horarios)
+  test 'editar horario de loja para horario inválido' do
+    loja = Loja.new abertura: '19:00', fechamento: '23:59'
+    assert loja.save
+    assert_not loja.update(fechamento: "18:00")
+  end
 end
