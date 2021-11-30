fcff784e2ee389edea50e99fd1d0f458754e6a34
Thiago Cavalcanti Silva <tcavalcanti.tc@gmail.com>
2021-11-29 21:30:21 -0300

    Gerando instancias diferentes nos testes unitarios de entregador

--- test/models/entregador_test.rb
+++ test/models/entregador_test.rb
@@ -3,20 +3,20 @@ require 'test_helper'
 class EntregadorTest < ActiveSupport::TestCase
   # Cadastro valido
   test 'entregador deve ser salvo' do
-    entregador = Entregador.new cpf: '70086904337', nome: 'Entregador de teste', telefone: '87999132767'
+    entregador = Entregador.new cpf: '70086904337', nome: 'Entregador de teste', telefone: '87999132760'
     assert entregador.save
   end
 
   # Edicao valida
   test 'entregador deve ser alterado' do
-    entregador = Entregador.new cpf: '70086904337', nome: 'Entregador de teste', telefone: '87999132767'
+    entregador = Entregador.new cpf: '70086904338', nome: 'Entregador', telefone: '87999132767'
     assert entregador.save
     assert entregador.update cpf: '60489003254', nome: 'Nome do entregador', telefone: '87999251454'
   end
 
   # Remocao
   test 'entregador deve ser removido' do
-    entregador = Entregador.new cpf: '70086904337', nome: 'Entregador de teste', telefone: '87999132767'
+    entregador = Entregador.new cpf: '70086904339', nome: 'Nome de teste', telefone: '87999132763'
     assert entregador.save
     assert entregador.delete
   end
