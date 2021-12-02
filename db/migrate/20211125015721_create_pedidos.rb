d440a7639dff86db28406d4059d775c03ad3ad5a
Luiz Davi <55983920+luiz-davi@users.noreply.github.com>
2021-12-02 20:55:12 -0300

    Modificando relação entre pedido e entregador

--- db/migrate/20211125015721_create_pedidos.rb
+++ db/migrate/20211125015721_create_pedidos.rb
@@ -2,7 +2,7 @@ class CreatePedidos < ActiveRecord::Migration[6.1]
   def change
     create_table :pedidos do |t|
       t.belongs_to :endereco, null: false, foreign_key: true
-      t.integer :entregador_id , foreign_key: true
+      t.belongs_to :entregador, null: false, foreign_key: true
       t.float :precoTotal
       t.string :status
       t.integer :quantidadePizzas
