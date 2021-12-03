e1db6826a3ea9956d02acdae0a37025c8a9dd800
jacksonlmp <jacksonlmp17.jl@gmail.com>
2021-12-02 21:59:47 -0300

    Validates de pizza

--- app/models/pizza.rb
+++ app/models/pizza.rb
@@ -4,5 +4,7 @@ class Pizza < ApplicationRecord
 
     # validate :checarSabores
     validates :tamanho, presence: true
-
+    validates :sabor1_id, presence: true
+    validates :sabor2_id, presence: true
+    validates :desc, length: { maximum: 40, too_long: "must have at maximum 40 characters" }
 end
