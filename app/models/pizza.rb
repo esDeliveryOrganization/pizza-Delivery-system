209a1832797dfaeade8155ff851c59559372dd7d
Luiz Davi <55983920+luiz-davi@users.noreply.github.com>
2021-12-04 19:10:58 -0300

    COnsertando validate de pizza: atributo 'desc' para 'observacao'

--- app/models/pizza.rb
+++ app/models/pizza.rb
@@ -6,5 +6,5 @@ class Pizza < ApplicationRecord
     validates :tamanho, presence: true
     validates :sabor1_id, presence: true
     validates :sabor2_id, presence: true
-    validates :desc, length: { maximum: 40, too_long: "must have at maximum 40 characters" }
+    validates :observacao, length: { maximum: 40, too_long: "must have at maximum 40 characters" }
 end
